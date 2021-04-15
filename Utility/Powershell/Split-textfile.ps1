f096ed9da7389bcfe1932cc7288b74cac4cf8bd9
Mikatgithub <32026996+Mikatgithub@users.noreply.github.com>
2021-04-15 16:11:01 +1000

    Update Split-textfile.ps1

--- Utility/Powershell/Split-textfile.ps1
+++ Utility/Powershell/Split-textfile.ps1
@@ -1,1 +1,1 @@
-$i = 0; gc "YourTextFile" -ReadCount 100000 |%{$i++; $_ | out-file "YourTextFile_Part$i.txt" -Encoding UTF8}
+$i = 0; gc "YourTextFile" -ReadCount 100000 |%{$i++; $_ | out-file 'YourTextFile_Part$i.txt' -Encoding UTF8}
