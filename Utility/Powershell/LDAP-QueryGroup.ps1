17c4dc8c9267437b98ab0a69e4bf7c9b51b38c76
Mikatgithub <32026996+Mikatgithub@users.noreply.github.com>
2021-05-26 15:50:44 +1000

    Create LDAP-QueryGroup.ps1

---
+++ Utility/Powershell/LDAP-QueryGroup.ps1
@@ -0,0 +1,9 @@
+$searcher = new-object system.DirectoryServices.Directorysearcher
+
+$searcher.filter = "(&(objectclass=group)(name=examplegroup))"
+
+$props = "name","distinguishedname","member"
+foreach($item in $props){
+$searcher.propertiestoload.add($item) | out-null
+}
+$searcher.findall()
