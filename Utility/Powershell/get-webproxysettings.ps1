6815bd1991a1d5a93ba02a71ee0484acf0990dab
Mikatgithub <32026996+Mikatgithub@users.noreply.github.com>
2021-05-27 10:08:03 +1000

    Create get-webproxysettings.ps1

---
+++ Utility/Powershell/get-webproxysettings.ps1
@@ -0,0 +1,2 @@
+get-itemproperty "hkcu:\software\Microsoft\windows\CurrentVersion\internet settings" -name proxyserver,autoconfigurl,proxyoverride
+get-itemproperty "hklm:\software\Microsoft\windows\CurrentVersion\internet settings" -name proxyserver,autoconfigurl,proxyoverride
