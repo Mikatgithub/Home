521cdf68b1c4b4a3c9bc0d5e1159da66c6dad0d1
Mikatgithub <32026996+Mikatgithub@users.noreply.github.com>
2021-09-21 13:52:34 +1000

    Create Set-AuRegionalSettings.ps1

---
+++ Utility/Powershell/Set-AuRegionalSettings.ps1
@@ -0,0 +1,5 @@
+#Sets Regional and Language settings for Win10 /Server 2016 to Australia and timezone to AEST
+Set-WinHomeLocation -GeoId 12
+Set-culture en-AU
+Set-winsystemlocale en-Au
+Set-timezone -id “AUS Eastern Standard Time”
